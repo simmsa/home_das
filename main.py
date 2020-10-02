275250e3a9b7f1f362e6a443f32ad3a97b7336c3
Andrew Simms <simms.andrew@gmail.com>
2020-10-01 20:02:43 -0600

    Fix: Wrap loop in "application" func for uwsgi

--- main.py
+++ main.py
@@ -89,7 +89,6 @@ def log_data():
     return True
 
 
-loop_time = 0
 one_sample_time = 1000000000 // data_rate_hz
 pump_gallons_per_minute = 43.5  # Per Anettes Engineering Docs
 pump_gallons_per_second = pump_gallons_per_minute / 60
@@ -116,192 +115,198 @@ def convert_raw_voltage_to_amps(raw_voltage):
     return raw_voltage * get_raw_to_voltage_to_amps_conversion_factor()
 
 
-conversion_factor = get_raw_to_voltage_to_amps_conversion_factor()
-samples = []
-sample_times = []
-data_collection_start = 0
-print("Monitoring data at {} sample(s) per second".format(data_rate_hz))
-print("Amperage conversion factor is: {}".format(conversion_factor))
-connection = db.connect(
-    os.path.join(base_dir, "home_das_db.db"),
-    detect_types=db.PARSE_DECLTYPES | db.PARSE_COLNAMES,
-)
-
-max_data_rate_hz_start = time.time_ns()
-max_data_rate_samples = 0
-for i in range(10):
-    acquire_data()
-    max_data_rate_samples = max_data_rate_samples + 1
-
-max_data_rate_hz_end = time.time_ns()
-average_ns_per_daq = (
-    max_data_rate_hz_end - max_data_rate_hz_start
-) / max_data_rate_samples
-
-print("Maximum Data rate is: {:.2f} hz".format(1_000_000_000 / average_ns_per_daq))
-
-now = datetime.now()
-# samples = 0
-with connection:
-    cursor = connection.cursor()
-    cursor.execute(
-        "CREATE TABLE IF NOT EXISTS SEPTIC_data(timestamp DATETIME, raw_sensor_voltage NUMERIC, amperage NUMERIC)"
-    )
-    cursor.execute(
-        "CREATE TABLE IF NOT EXISTS WATER_USAGE_DATA(timestamp DATETIME, gallons_pumped NUMERIC)"
+def application():
+    loop_time = 0
+    conversion_factor = get_raw_to_voltage_to_amps_conversion_factor()
+    samples = []
+    sample_times = []
+    data_collection_start = 0
+    print("Monitoring data at {} sample(s) per second".format(data_rate_hz))
+    print("Amperage conversion factor is: {}".format(conversion_factor))
+    connection = db.connect(
+        os.path.join(base_dir, "home_das_db.db"),
+        detect_types=db.PARSE_DECLTYPES | db.PARSE_COLNAMES,
     )
 
-    startup_file = open(os.path.join(base_dir, "home_das.log"), "a")
-    startup_file.write("{}\n".format("Starting Data Monitoring..."))
-    startup_file.close()
+    max_data_rate_hz_start = time.time_ns()
+    max_data_rate_samples = 0
+    for i in range(10):
+        acquire_data()
+        max_data_rate_samples = max_data_rate_samples + 1
 
-    while True:
-        if has_time_passed(loop_time):
-            # print(
-            #     "Current Data Rate: {} hz".format(
-            #         1_000_000_000 / (time.time_ns() - loop_time)
-            #     )
-            # )
-            # print(time.time_ns() - loop_time)
-            # start = time.time_ns()
-            daq_time = time.time_ns()
-            data = acquire_data()
+    max_data_rate_hz_end = time.time_ns()
+    average_ns_per_daq = (
+        max_data_rate_hz_end - max_data_rate_hz_start
+    ) / max_data_rate_samples
 
-            if data > data_collection_voltage_threshold:
-                if data_collection_start == 0:
-                    now = datetime.now()
-                    data_collection_start = daq_time
-                samples.append(data)
-                sample_times.append(daq_time)
-            else:
-                if len(samples) > 0:
-                    compute_start = time.time_ns()
-                    # Parse the data, save it, clear it
-                    # Parse
-                    # seconds = len(samples) / 30
-                    seconds = (time.time_ns() - data_collection_start) / 1000000000
-                    raw_samples = copy.copy(samples)
-                    samples = np.array(samples)
-                    # Convert everything to amperage
-                    samples = samples * conversion_factor
-                    max_amps = np.max(samples)
-                    average_amps = np.average(samples)
-                    start_time = now.strftime("%Y%m%d-%H:%M:%S")
-                    pumped_gallons = (
-                        seconds * pump_gallons_per_second
-                    ) - transport_volume
+    print("Maximum Data rate is: {:.2f} hz".format(1_000_000_000 / average_ns_per_daq))
 
-                    # Save
-                    log_text = "{}: Dosing pump ran for {:.2f} seconds, pumped {:.2f} gallons with a max amperage of {:.2f}A, an average amperage of {:.2f}A, and an average wattage of {:.2f}W".format(
-                        start_time,
-                        seconds,
-                        pumped_gallons,
-                        max_amps,
-                        average_amps,
-                        average_amps * 120.0,
-                    )
-                    print(log_text)
+    now = datetime.now()
+    # samples = 0
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS SEPTIC_data(timestamp DATETIME, raw_sensor_voltage NUMERIC, amperage NUMERIC)"
+        )
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS WATER_USAGE_DATA(timestamp DATETIME, gallons_pumped NUMERIC)"
+        )
 
-                    # log_file = open("home_das.log", "a")
-                    log_file = open(os.path.join(base_dir, "home_das.log"), "a")
-                    log_file.write("{}\n".format(log_text))
+        startup_file = open(os.path.join(base_dir, "home_das.log"), "a")
+        startup_file.write("{}\n".format("Starting Data Monitoring..."))
+        startup_file.close()
 
-                    np.savetxt(
-                        os.path.join(base_dir, "{}.csv".format(start_time)),
-                        samples,
-                        delimiter=",",
-                    )
-                    np.savetxt(
-                        os.path.join(base_dir, "RAW_{}.csv".format(start_time)),
-                        raw_samples,
-                        delimiter=",",
-                    )
-                    np.savetxt(
-                        os.path.join(base_dir, "NS_{}.csv".format(start_time)),
-                        sample_times,
-                        delimiter=",",
-                    )
+        while True:
+            if has_time_passed(loop_time):
+                # print(
+                #     "Current Data Rate: {} hz".format(
+                #         1_000_000_000 / (time.time_ns() - loop_time)
+                #     )
+                # )
+                # print(time.time_ns() - loop_time)
+                # start = time.time_ns()
+                daq_time = time.time_ns()
+                data = acquire_data()
 
-                    plt.plot(samples)
-                    plt.ylabel("Amps")
-                    plt.title("Septic Pump Run - {}".format(start_time))
-                    plt.savefig(
-                        os.path.join(base_dir, "Amperage-{}.png".format(start_time))
-                    )
-                    plt.close()
+                if data > data_collection_voltage_threshold:
+                    if data_collection_start == 0:
+                        now = datetime.now()
+                        data_collection_start = daq_time
+                    samples.append(data)
+                    sample_times.append(daq_time)
+                else:
+                    if len(samples) > 0:
+                        compute_start = time.time_ns()
+                        # Parse the data, save it, clear it
+                        # Parse
+                        # seconds = len(samples) / 30
+                        seconds = (time.time_ns() - data_collection_start) / 1000000000
+                        raw_samples = copy.copy(samples)
+                        samples = np.array(samples)
+                        # Convert everything to amperage
+                        samples = samples * conversion_factor
+                        max_amps = np.max(samples)
+                        average_amps = np.average(samples)
+                        start_time = now.strftime("%Y%m%d-%H:%M:%S")
+                        pumped_gallons = (
+                            seconds * pump_gallons_per_second
+                        ) - transport_volume
 
-                    # Water Usage
-                    cursor.execute(
-                        "INSERT INTO WATER_USAGE_DATA(timestamp, gallons_pumped) VALUES(?, ?)",
-                        (now, pumped_gallons),
-                    )
-
-                    cursor.execute(
-                        "SELECT * FROM WATER_USAGE_DATA",
-                    )
-                    # What order are these in?
-                    water_data = cursor.fetchall()
-                    print("Water data: ", water_data)
-                    water_data_timestamps = [i[0] for i in water_data]
-                    water_data_gallons_pumped = [i[1] for i in water_data]
-                    water_data_gallons_pumped = np.cumsum(water_data_gallons_pumped)
-                    print("Pump Timestamps: ", water_data_timestamps)
-                    print("Gallons pumped: ", water_data_gallons_pumped)
-
-                    plt.plot(water_data_timestamps, water_data_gallons_pumped)
-                    plt.ylabel("Gallons")
-                    plt.title(
-                        "Water Usage: {} - {}".format(
-                            water_data_timestamps[0], water_data_timestamps[-1]
+                        # Save
+                        log_text = "{}: Dosing pump ran for {:.2f} seconds, pumped {:.2f} gallons with a max amperage of {:.2f}A, an average amperage of {:.2f}A, and an average wattage of {:.2f}W".format(
+                            start_time,
+                            seconds,
+                            pumped_gallons,
+                            max_amps,
+                            average_amps,
+                            average_amps * 120.0,
                         )
-                    )
-                    plt.savefig(
-                        os.path.join(base_dir, "WaterUsage-{}.png".format(start_time))
-                    )
-                    plt.close()
+                        print(log_text)
 
-                    # Data Analysis
-                    time_btw_samples = np.diff(np.array(sample_times))
-                    data_analysis_text = "The average time between samples is: {}ns, std dev is: {}ns, it should be {}ns".format(
-                        np.average(time_btw_samples),
-                        np.std(time_btw_samples),
-                        one_sample_time,
-                    )
-                    log_file.write("{}\n".format(data_analysis_text))
+                        # log_file = open("home_das.log", "a")
+                        log_file = open(os.path.join(base_dir, "home_das.log"), "a")
+                        log_file.write("{}\n".format(log_text))
 
-                    plt.plot(sample_times)
-                    plt.ylabel("Sample Time (ns)")
-                    plt.title("Sample Times - {}".format(start_time))
-                    plt.savefig(
-                        os.path.join(base_dir, "SampleTimes-{}.png".format(start_time))
-                    )
-                    plt.close()
-
-                    compute_end = time.time_ns()
-                    compute_log = (
-                        "Parsing, Logging, Saving, and Graphing took {} ms".format(
-                            (compute_end - compute_start) / 1000000
+                        np.savetxt(
+                            os.path.join(base_dir, "{}.csv".format(start_time)),
+                            samples,
+                            delimiter=",",
                         )
-                    )
-                    print(compute_log)
-                    log_file.write("{}\n".format(compute_log))
-                    log_file.close()
+                        np.savetxt(
+                            os.path.join(base_dir, "RAW_{}.csv".format(start_time)),
+                            raw_samples,
+                            delimiter=",",
+                        )
+                        np.savetxt(
+                            os.path.join(base_dir, "NS_{}.csv".format(start_time)),
+                            sample_times,
+                            delimiter=",",
+                        )
 
-                    # Clear
-                    samples = []
-                    sample_times = []
-                    data_collection_start = 0
-                    # samples = np.array(samples)
-                    # samples_min = np.min(samples)
-                    # samples_max = np.max(samples)
-                    # sample_times = np.array(sample_times)
-            loop_time = time.time_ns()
+                        plt.plot(samples)
+                        plt.ylabel("Amps")
+                        plt.title("Septic Pump Run - {}".format(start_time))
+                        plt.savefig(
+                            os.path.join(base_dir, "Amperage-{}.png".format(start_time))
+                        )
+                        plt.close()
 
-            # print(data)
-            # cursor.execute("INSERT INTO SEPTIC_data values(datetime('now'), (?), (?))", (data, data * 2))
-            # samples += 1
-            # if samples % 1000 == 0:
-            #     print("Acquired {} samples!".format(samples))
-            # print("Data Acquired in {} ms".format((time.time_ns() - start) / 1000000))
-            # print(time.time_ns() - loop_time)
-            # print(one_sample_time)
+                        # Water Usage
+                        cursor.execute(
+                            "INSERT INTO WATER_USAGE_DATA(timestamp, gallons_pumped) VALUES(?, ?)",
+                            (now, pumped_gallons),
+                        )
+
+                        cursor.execute(
+                            "SELECT * FROM WATER_USAGE_DATA",
+                        )
+                        # What order are these in?
+                        water_data = cursor.fetchall()
+                        print("Water data: ", water_data)
+                        water_data_timestamps = [i[0] for i in water_data]
+                        water_data_gallons_pumped = [i[1] for i in water_data]
+                        water_data_gallons_pumped = np.cumsum(water_data_gallons_pumped)
+                        print("Pump Timestamps: ", water_data_timestamps)
+                        print("Gallons pumped: ", water_data_gallons_pumped)
+
+                        plt.plot(water_data_timestamps, water_data_gallons_pumped)
+                        plt.ylabel("Gallons")
+                        plt.title(
+                            "Water Usage: {} - {}".format(
+                                water_data_timestamps[0], water_data_timestamps[-1]
+                            )
+                        )
+                        plt.savefig(
+                            os.path.join(
+                                base_dir, "WaterUsage-{}.png".format(start_time)
+                            )
+                        )
+                        plt.close()
+
+                        # Data Analysis
+                        time_btw_samples = np.diff(np.array(sample_times))
+                        data_analysis_text = "The average time between samples is: {}ns, std dev is: {}ns, it should be {}ns".format(
+                            np.average(time_btw_samples),
+                            np.std(time_btw_samples),
+                            one_sample_time,
+                        )
+                        log_file.write("{}\n".format(data_analysis_text))
+
+                        plt.plot(sample_times)
+                        plt.ylabel("Sample Time (ns)")
+                        plt.title("Sample Times - {}".format(start_time))
+                        plt.savefig(
+                            os.path.join(
+                                base_dir, "SampleTimes-{}.png".format(start_time)
+                            )
+                        )
+                        plt.close()
+
+                        compute_end = time.time_ns()
+                        compute_log = (
+                            "Parsing, Logging, Saving, and Graphing took {} ms".format(
+                                (compute_end - compute_start) / 1000000
+                            )
+                        )
+                        print(compute_log)
+                        log_file.write("{}\n".format(compute_log))
+                        log_file.close()
+
+                        # Clear
+                        samples = []
+                        sample_times = []
+                        data_collection_start = 0
+                        # samples = np.array(samples)
+                        # samples_min = np.min(samples)
+                        # samples_max = np.max(samples)
+                        # sample_times = np.array(sample_times)
+                loop_time = time.time_ns()
+
+                # print(data)
+                # cursor.execute("INSERT INTO SEPTIC_data values(datetime('now'), (?), (?))", (data, data * 2))
+                # samples += 1
+                # if samples % 1000 == 0:
+                #     print("Acquired {} samples!".format(samples))
+                # print("Data Acquired in {} ms".format((time.time_ns() - start) / 1000000))
+                # print(time.time_ns() - loop_time)
+                # print(one_sample_time)
