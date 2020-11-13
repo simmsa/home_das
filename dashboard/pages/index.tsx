6dd78b52a77fc95cf03f0fb2a911a3f15ad5044d
Andrew Simms <simms.andrew@gmail.com>
2020-11-12 20:33:00 -0700

    Feature: Add nivo responsive calendar

--- dashboard/pages/index.tsx
+++ dashboard/pages/index.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 import { formatDistance } from "date-fns";
 import parse from "date-fns/parse";
+import format from "date-fns/format";
 import sqlite3 from "sqlite3";
+import { ResponsiveCalendar } from "@nivo/calendar";
 
 import Head from "next/head";
 
@@ -33,80 +35,41 @@ type HomeProps = {
 
 // <div>{formatDistance(timestampTime, new Date())}</div>
 function Home({ dosingPumpRecords }: HomeProps) {
+  const calendarData = dosingPumpRecords.map((record: DosingPumpRecord) => {
+    const timestampTime = parse(
+      record.timestamp.split(".")[0],
+      "yyyy-MM-dd HH:mm:ss",
+      new Date()
+    );
+    const timeForCal = format(timestampTime, "yyyy-MM-dd");
+    return {
+      day: timeForCal,
+      value: record.gallons_pumped
+    };
+  });
   return (
     <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <div>
-          {dosingPumpRecords.map((record: DosingPumpRecord) => {
-            const timestampTime = parse(
-              record.timestamp.split(".")[0],
-              "yyyy-MM-dd HH:mm:ss",
-              new Date()
-            );
-            console.log(timestampTime);
-            return (
-              <div key={record.timestamp}>
-                <div>{`${JSON.stringify(record)}`}</div>
-                <div>{formatDistance(timestampTime, new Date())}</div>
-                <div>{`${record.gallons_pumped.toFixed(2)} G`}</div>
-              </div>
-            );
-          })}
-        </div>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
+      <ResponsiveCalendar
+        data={calendarData}
+        from={calendarData[0].day}
+        to={calendarData[calendarData.length - 1].day}
+      />
+      <div>
+        {dosingPumpRecords.map((record: DosingPumpRecord) => {
+          const timestampTime = parse(
+            record.timestamp.split(".")[0],
+            "yyyy-MM-dd HH:mm:ss",
+            new Date()
+          );
+          console.log(timestampTime);
+          return (
+            <div key={record.timestamp}>
+              <div>{`${formatDistance(timestampTime, new Date())} ago`}</div>
+              <div>{`${record.gallons_pumped.toFixed(2)} G`}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
