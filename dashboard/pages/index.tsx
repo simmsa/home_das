cc3e353bcdf4f58dff65930384d74681b9546bf6
Andrew Simms <simms.andrew@gmail.com>
2020-11-13 21:15:04 -0700

    UI: Usability tweaks

--- dashboard/pages/index.tsx
+++ dashboard/pages/index.tsx
@@ -79,7 +79,7 @@ function Home({ dosingPumpRecords }: HomeProps) {
   console.log(dayOfWeekDict);
   return (
     <div className="container">
-      <h1>Dosing Tank Monitor</h1>
+      <h1>Dosing Tank Recorder</h1>
       <h3>Gallons Pumped Per Day Of Week</h3>
 
       <BarChart
@@ -100,7 +100,7 @@ function Home({ dosingPumpRecords }: HomeProps) {
         <Bar dataKey="value" fill="#1E88E5" />
       </BarChart>
       <h3>Gallons Pumped Per Day Of Year</h3>
-      <div style={{ height: "200px", width: "800px" }}>
+      <div style={{ height: "200px", width: "800px", paddingTop: "15px" }}>
         <ResponsiveCalendar
           data={formattedCalData}
           from={completeCalData[0].day}
