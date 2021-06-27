f59cb21a8bd22fd0c3e9d591df88093042bf2837
Andrew Simms <simms.andrew@gmail.com>
2021-06-27 06:51:52 -0600

    UI: Improve font and icon loading
    Warm up the dns cache for the google fonts css request

--- dashboard/pages/index.tsx
+++ dashboard/pages/index.tsx
@@ -480,6 +480,7 @@ class Home extends React.Component<HomeStyledProps, HomeState> {
         <div>
           <Head>
             <title>Dosing Pump Records</title>
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link
               rel="stylesheet"
               href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
